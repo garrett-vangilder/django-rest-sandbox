815953d5ebae367fed08b6f125b549e0fbed6d49
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-09-20 20:23:36 -0400

    adds profile feed item migrations and admin

--- profiles-rest-api/src/profiles_project/profiles_api/models.py
+++ profiles-rest-api/src/profiles_project/profiles_api/models.py
@@ -49,3 +49,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """"Django uses this when it needs to convert the object when its a string"""
         return str(self.email)
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update."""
+
+    user_profile = models.ForeignKey(
+        'UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """return the model as a string"""
+        return self.status_text

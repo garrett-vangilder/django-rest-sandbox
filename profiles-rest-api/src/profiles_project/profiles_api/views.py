87c195f60ed588c4f68a3bc93c4673a395bd01da
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-09-20 20:01:28 -0400

    adds search feature

--- profiles-rest-api/src/profiles_project/profiles_api/views.py
+++ profiles-rest-api/src/profiles_project/profiles_api/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
@@ -99,3 +99,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
